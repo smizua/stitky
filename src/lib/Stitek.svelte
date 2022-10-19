96cb064bf5b5cfa49374c8016707a730494df2fb
smizua <116209392+smizua@users.noreply.github.com>
2022-10-20 01:07:08 +0200

    sveltekit

---
+++ src/lib/Stitek.svelte
@@ -0,0 +1,18 @@
+<script>
+  import Logo from './logo.svelte';
+</script>
+
+<div class="stitek">
+  Stitek!
+  <div>
+    <Logo />
+  </div>
+</div>
+
+<style>
+  .stitek {
+    width: 7cm;
+    height: 5cm;
+    border: 1px #666 dashed;
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+  import Stitek from '$lib/Stitek.svelte';
+
+  let pocet = 1;
+
+  $: stitky = new Array(pocet);
+</script>
+
+<div class="no-print">
+  <input type="number" bind:value={pocet} />
+</div>
+
+{#each stitky as stitek}
+  <Stitek />
+{/each}
+
+<style>
+  @media print {
+    .no-print {
+      visibility: hidden;
+      display: none;
+    }
+  }
+</style>
