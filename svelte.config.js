27086eafec327543c236b2b33116ee3995e0a21a
Pavel Lang <langpavel@phpskelet.org>
2022-10-21 01:52:10 +0200

    enable inspector

--- svelte.config.js
+++ svelte.config.js
@@ -3,13 +3,28 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter()
+  },
+
+  vitePlugin: {
+    experimental: {
+      inspector: {
+        // change shortcut
+        toggleKeyCombo: 'control',
+        // hold and release key to toggle inspector mode
+        holdMode: true,
+        // show or hide the inspector option
+        showToggleButton: 'always',
+        // inspector position
+        toggleButtonPos: 'top-right'
+      }
+    }
+  }
 };
 
 export default config;
