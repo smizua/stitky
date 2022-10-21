48f72339f765f3194bae236acbc3ba1205a48860
Pavel Lang <langpavel@phpskelet.org>
2022-10-21 10:12:06 +0200

    add compiler

---
+++ src/routes/compiler/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { compile } from 'svelte/compiler';
+
+  let value: string = '';
+  let result: any;
+  let error: string = '';
+
+  $: try {
+    result = compile(value);
+    error = '';
+  } catch (err: any) {
+    result = undefined;
+    error = err.toString();
+  }
+</script>
+
+<textarea bind:value cols="80" rows="20" />
+
+<br />
+
+<div>
+  <pre>{error}{JSON.stringify(result, null, 2)}
+  </pre>
+</div>
