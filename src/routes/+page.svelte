94167eb0ee562347e4e6de9477652f046927b85f
Pavel Lang <langpavel@phpskelet.org>
2022-10-30 23:19:30 +0100

    pokus

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,47 @@
 <script>
   import Stitek from '$lib/Stitek.svelte';
+  import Logo from '$lib/logo.svelte';
+  import produkty from '$lib/produkty.json';
 
-  let pocet = 1;
+  const velikosti = new Set();
+  for (const { velikostStitku } of produkty) {
+    velikosti.add(velikostStitku);
+  }
 
-  $: stitky = new Array(pocet);
+  const velikostToClassName = {
+    šťáva: 'stava',
+    štítek: 'stitek',
+    tinktura: 'tinktura'
+  };
 </script>
 
 <div class="no-print">
-  <input type="number" bind:value={pocet} />
+  <h1>Netiskne se</h1>
+
+  <!-- velikosti:
+  {#each Array.from(velikosti) as velikost}
+    <p>{velikost}</p>
+  {/each} -->
 </div>
 
-{#each stitky as stitek}
-  <Stitek />
+{#each produkty as produkt}
+  <!-- <Stitek /> -->
+  <div class="stitek {velikostToClassName[produkt.velikostStitku]}">
+    <div>
+      <div class="name">{produkt.name}</div>
+      <div class="latinName">{produkt.latinName}</div>
+      <div class="logo">
+        <div class="logo-inner"><Logo /></div>
+      </div>
+      <div class="textSuvenyr">{produkt.textSuvenyr}</div>
+    </div>
+    <div class="side">
+      <div class="side2">
+        <div class="textObsah">Obsah: {produkt.textObsah}</div>
+        <div class="textVyrobeno">Vyrobeno: {produkt.textVyrobeno}</div>
+      </div>
+    </div>
+  </div>
 {/each}
 
 <style>
@@ -20,5 +50,60 @@
       visibility: hidden;
       display: none;
     }
+  }
+
+  .stitek {
+    display: inline-block;
+    position: relative;
+    border: 1px dashed #ccc;
+  }
+
+  .logo-inner {
+    display: inline;
+    text-align: center;
+  }
+
+  .stitek.stava {
+    width: 10.5cm;
+    height: 6.5cm;
+  }
+  .stitek.stitek {
+    width: 10.5cm;
+    height: 6.5cm;
+  }
+  .stitek.tinktura {
+    width: 10.5cm;
+    height: 6.5cm;
+  }
+
+  .name {
+    text-align: center;
+  }
+  .latinName {
+    text-align: center;
+  }
+  .textObsah {
+    text-align: center;
+  }
+  .textSuvenyr {
+    text-align: center;
+  }
+  .textVyrobeno {
+    text-align: center;
+  }
+
+  .side {
+    background-color: bisque;
+    position: absolute;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    width: 1.5cm;
+  }
+  .side2 {
+    background-color: blue;
+    height: 4.5cm;
+    text-align: center;
+    /* transform: rotate(-90deg); */
   }
 </style>
